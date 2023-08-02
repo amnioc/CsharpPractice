6bf7d420705c3ca52b65ad61e70737137b3bfd50
amnioc <nicdhillon19@gmail.com>
2023-08-02 15:11:32 +0100

    .NET Library practice

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//.NET Library Practice
+
+// FIRST TEST
+// Random dice = new Random();
+// int roll = dice.Next(1,7); // random.Next generates a number
+// Console.WriteLine(roll); // displays number
+
+// OVERLOAD METHODS
+// Random dice = new Random();
+// int roll1 = dice.Next();
+// int roll2 = dice.Next(101);
+// int roll3 = dice.Next(50, 101);
+
+// Console.WriteLine($"First roll is {roll1}");
+// Console.WriteLine($"Second roll is {roll2}");
+// Console.WriteLine($"Third roll is {roll3}");
+
+// dice.Next()
+
+//MATH CLASS METHODS CHALLENGE
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+
+Console.WriteLine(largerValue);
+
